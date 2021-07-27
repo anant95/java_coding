ba19ee1fcb8770668b32e9c2660b18445d0a25f8
anantkum <anant.kumar@intel.com>
2021-07-27 12:40:39 +0530

    Array dominent index and plusOne

--- leetcode/arrayList.java
+++ leetcode/arrayList.java
@@ -9,7 +9,9 @@ public class arrayList{
 			a1[i]= sc.nextInt();
 		}
 		arrayList al = new arrayList();
-		System.out.printf("%d ", al.pivotIndex(a1));
+		int[] ans = al.plusOne(a1);
+		for(int i = 0; i<ans.length; i++)
+			System.out.printf("%d ", ans[i]);
 	}
 	public int pivotIndex(int[] nums) {
 		int n = nums.length;
@@ -28,4 +30,61 @@ public class arrayList{
 		}
 			return -1;
 	}
+	public int dominantIndex(int[] nums) {
+		int n = nums.length;
+		if(n == 1){
+			return 0;
+		}
+		int l = nums[0];
+		int s = nums[1];
+		int index = 0;
+		if(nums[0]<nums[1]){
+			l = nums[1];
+			s = nums[0];
+			index = 1;
+		}
+		for(int i = 2; i<n; i++){
+			if(l<=nums[i]){
+				s = l;
+				l = nums[i];
+				index = i;
+			}
+			else if(s<nums[i])
+				s = nums[i];
+		}
+		if(l>=(2*s))
+			return index;
+		else
+			return -1;
+    }
+    public int[] plusOne(int[] digits) {
+	    int n = digits.length;
+	    int carry = 0;
+	    if(digits[n-1]+1+carry == 10){
+		    carry = 1;
+		    digits[n-1] = 0;
+	    }
+	    else{
+		    digits[n-1] = digits[n-1]+carry+1;
+		    carry = 0;
+		    return digits;
+	    }
+	    for(int i = n-2; i>=0; i--){
+		    if(digits[i]+carry == 10){
+			    carry = 1;
+			    digits[i] = 0;
+		    }
+		    else{
+			    digits[i] = digits[i]+carry;
+			    carry = 0;
+			    break;
+		    }
+	    }
+	    if(carry == 1){
+		    int[] ans = new int[n+1];
+		    ans[0] = 1;
+		    return ans;
+	    }
+	    return digits;
+    }
 }

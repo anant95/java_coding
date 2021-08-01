427dcbe2da19dc3bcd62d58eb626fd64857d2258
anantkum <anant.kumar@intel.com>
2021-08-01 22:47:04 +0530

    DIVIDE AND CONQUR LONGESTcOMMONpREFIX

--- leetcode/string.java
+++ leetcode/string.java
@@ -63,4 +63,29 @@ class string{
 	     }
 	     return -1;
 	}
+	public String longestCommonPrefix(String[] strs) {
+		if(strs == null || strs.length ==0) return "";
+		return longestCommonPrefix(strs, 0 , strs.length - 1);
+    }
+    public String longestCommonPrefix(String[] strs, int l, int r) {
+    if (l == r) {
+        return strs[l];
+    }
+    else {
+	    string st = new string();
+	    int mid = (l+r)/2;
+	    String left = longestCommonPrefix(strs, l, mid);
+	    String right = longestCommonPrefix(strs, mid+1, r);
+	    return commonPrefix(left, right);
+    }
+    }
+    String commonPrefix(String left,String right) {
+	    int min = Math.min(left.length(), right.length());
+	    for(int i = 0; i<min; i++){
+		    if(left.charAt(i) != right.charAt(i))
+			    return left.substring(0, i);
+	    }
+	    return left.substring(0, min);
+    }
+
 }
